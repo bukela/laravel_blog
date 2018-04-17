2cc0f4bcc640307ea04429202e12df5bcba63a8e
bukela <bukela@gmail.com>
2018-04-17 16:55:31 +0200

    wysiwyg editor 2

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -51,7 +51,7 @@
                 </div>  
             <div class="form-group">
                 <label for="">Content</label>
-            <textarea name="content" id="" cols="5" rows="5" class="form-control">{{ $post->content }}</textarea>
+            <textarea name="content" id="summernote" cols="5" rows="5" class="form-control">{{ $post->content }}</textarea>
             </div>
             <div class="form-group">
                 <div class="text-center">
@@ -61,4 +61,15 @@
         </form>
     </div>
 </div>
+@endsection
+@section('styles')
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
+@endsection
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
+</script>
 @endsection
